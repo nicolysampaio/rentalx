5b5eb3d7dd963f025086fcb20aafbbccb19024e1
nicolysampaio <78435150+nicolysampaio@users.noreply.github.com>
2022-05-31 00:11:13 -0300

    add name validation

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -7,6 +7,10 @@ const categoriesRespository = new CategoriesRespository();
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
+  const categoryAlreadyExists = categoriesRespository.findByName(name);
+
+  if(categoryAlreadyExists) return response.status(400).json({error: 'Category already exists'});
+
   categoriesRespository.create({ name, description });
 
   return response.status(201).send();
