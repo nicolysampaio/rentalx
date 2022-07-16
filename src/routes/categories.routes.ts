60a3cd8676b4af6a26e7b8966075c5bb8613fc92
Nicoly Sampaio <78435150+nicolysampaio@users.noreply.github.com>
2022-07-16 19:07:45 -0300

    docs: swagger update

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import multer from "multer";
 
-
 import { CreateCategoryController } from './../modules/cars/useCases/createCategory/CreateCategoryController';
 import { ImportCategoryController } from '../modules/cars/useCases/importCategory/ImportCategoryController';
 import { ListCategoriesController } from "../modules/cars/useCases/listCategories/ListCategoriesController";
